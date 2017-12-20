8d68b1c7c2c370a23e993fc6cf32fc0ca7762850
Morgan Gardiner <scily98@gmail.com>
2017-12-20 23:16:22 +0000

    first update

--- WorkDictionary/ViewController.swift
+++ WorkDictionary/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableview1: UITableView!
+   
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
